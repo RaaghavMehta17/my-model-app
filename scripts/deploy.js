38c81867c596a8bbf670b99b85df542d080062ca
“Raaghav <raaghavmehta17@gmail.com>
2024-07-21 21:15:07 +0530

    add wallet contract

---
+++ scripts/deploy.js
@@ -0,0 +1,17 @@
+const hre = require("hardhat");
+
+async function main() {
+  const Wallet = await hre.ethers.getContractFactory("Wallet");
+  const wallet = await Wallet.deploy();
+
+  await wallet.deployed();
+
+  console.log("Wallet deployed to:", wallet.address);
+}
+
+main()
+  .then(() => process.exit(0))
+  .catch((error) => {
+    console.error(error);
+    process.exit(1);
+  });
